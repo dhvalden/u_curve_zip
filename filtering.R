6c5b8925d503f21a66a8b20b52c87d629e62558b
Daniel Valdenegro <dhvalden@gmail.com>
2020-08-12 21:05:24 -0400

    filtering recieved zip data under resgistered criteria

---
+++ filtering.R
@@ -0,0 +1,30 @@
+library(psych)
+
+df <- read.csv2('data/zipdata.csv')
+
+str(df)
+
+## removing attention check fails
+
+fltr <- df$att1 == 2 & df$att2 == 6
+fltr[is.na(fltr)] <- FALSE
+clean_df <- df[fltr, ]
+clean_df$X <- NULL
+str(clean_df)
+
+## removing samples with less than 15
+
+sample_size <- as.data.frame(table(clean_df$Sample))
+colnames(sample_size) <- c('Sample', 'sample_size')
+
+clean_df <- merge(clean_df, sample_size, by='Sample')
+clean_df <- clean_df[clean_df$sample_size >= 15, ]
+
+## sanity checks
+dim(clean_df)
+table(clean_df$Sample)
+length(unique(clean_df$Sample))
+
+## Saving as cleanzip.csv
+
+write.csv(clean_df, 'data/cleanzip.csv')
